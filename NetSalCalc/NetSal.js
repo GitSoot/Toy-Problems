62abc226d31055e02b91fd7efec889cf8f6525b8
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-11-28 02:32:36 +0300

    updates on conditionals for salary categories.

--- NetSalCalc/NetSal.js
+++ NetSalCalc/NetSal.js
@@ -14,3 +14,21 @@ const ownerOccupierInterest = 25000;
 const ownerOccupierInterestYear = ownerOccupierInterest * 12;
 const disabilityExemption = 150000;
 const disabilityExemptionYear = disabilityExemption * 12;
+
+function netsal(basicSalary) {
+    let salary;
+    if (basicSalary < 6000) {
+        salary = basicSalary - 150;
+
+    } else if (basicSalary > 6000 && basicSalary < 8000) {
+        salary = basicSalary - 300;
+    } else if (basicSalary > 8000 && basicSalary < 11999) {
+        salary = basicSalary - 400;
+    } else if (basicSalary > 12000 && basicSalary < 14999) {
+        salary = basicSalary - 500;
+    }
+    return salary
+}
+
+let netsalary = netsal;
+netsal(basicSalary)
