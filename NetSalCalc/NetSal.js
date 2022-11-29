688fa28d1df64c3f9f946d32ce1272d3372dbbaa
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-11-29 09:48:20 +0300

    Updated to include deductions + salary

--- NetSalCalc/NetSal.js
+++ NetSalCalc/NetSal.js
@@ -8,33 +8,60 @@ const personalRelief = 2400;
 const insuranceRelief = 5000;
 const pensionFund = 20000;
 const housingRelief = 9000;
-const relief = personalRelief + insuranceRelief + housingRelief;
+const relief = personalRelief + insuranceRelief + housingRelief + pensionFund;
 //net salary function
 function netsal(basicSalary) {
     let salary;
+    let nhif;
+    let tax;
+    let nssf;
     if (basicSalary < 6000) {
-        salary = basicSalary - 150;
+        nssf = 200;
+        nhif = 200;
+        salary = (basicSalary + relief) - (nssf + nhif);
 
     } else if (basicSalary > 6000 && basicSalary < 8000) {
-        salary = basicSalary - 300;
+        nssf = 200;
+        nhif = 300;
+        salary = (basicSalary + relief) - (nssf + nhif);
     } else if (basicSalary > 8000 && basicSalary < 11999) {
-        salary = basicSalary - 400;
+        nssf = 300;
+        nhif = 400;
+        salary = (basicSalary + relief) - (nssf + nhif);
     } else if (basicSalary > 12000 && basicSalary < 14999) {
-        salary = basicSalary - 500;
+        nssf = 400;
+        nhif = 500;
+        salary = (basicSalary + relief) - (nssf + nhif);
     } else if (basicSalary > 15000 && basicSalary < 19999) {
-        salary = basicSalary - 600;
+        nssf = 400;
+        nhif = 600;
+        salary = basicSalary - (nssf + nhif);
     } else if (basicSalary > 20000 && basicSalary < 29999) {
-        salary = basicSalary - 850;
+        nssf = 1000;
+        nhif = 900;
+        salary = (basicSalary + relief) - (nssf + nhif);
     } else if (basicSalary > 30000 && basicSalary < 69999) {
-        salary = (paye1 + relief) - 1100;
+        nhif = 1100;
+        nssf = basicSalary * 0.04;
+        tax = basicSalary - paye1;
+        salary = (paye1 + relief) - (nhif + nssf);
     } else if (basicSalary > 70000 && basicSalary < 129999) {
-        salary = (paye2 + relief) - 1700;
+        nhif = 1100;
+        nssf = basicSalary * 0.04;
+        tax = basicSalary - paye1;
+        salary = (paye1 + relief) - (nhif + nssf);
     } else if (basicSalary > 130000 && basicSalary < 199999) {
-        salary = (paye3 + relief) - 2000;
+        nhif = basicSalary * 0.06;
+        nssf = basicSalary * 0.04;
+        tax = basicSalary - paye1;
+        salary = (paye1 + relief) - (nhif + nssf);
     } else {
-        salary = (paye4 + relief) - (pensionFund + 2300)
+        nhif = basicSalary * 0.08;
+        nssf = basicSalary * 0.04;
+        tax = basicSalary - paye1;
+        salary = (paye1 + relief) - (nhif + nssf);
     }
-    return salary
+    return [`net salary = ${salary},  gross salary = ${basicSalary}, NHIF deductions = ${nhif}, PAYE = ${tax}, NSSF deductions = ${nssf}`]
 }
 
 let netsalary = netsal;
